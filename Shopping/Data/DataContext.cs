13152164ab486fa214b0ddeb3c7a51caa66053d0
tomasotano25 <tomasotano25@gmail.com>
2022-03-18 13:34:15 -0400

    Categories CRUD finished

--- Shopping/Data/DataContext.cs
+++ Shopping/Data/DataContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Shopping.Data.Entities;
 using Shopping.Data.Entities.Directory;
 
 namespace Shopping.Data
@@ -12,11 +13,15 @@ namespace Shopping.Data
 
         public DbSet<Country> Countries { get; set; }
 
+        public DbSet<Category> Categories { get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
 
             modelBuilder.Entity<Country>().HasIndex(c => c.Name).IsUnique();
+
+            modelBuilder.Entity<Category>().HasIndex(c => c.Name).IsUnique();
         }
     }
 }
